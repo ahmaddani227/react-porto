64ca38a7b0411e769b3c4b563c8aa6727ef31afe
ahmaddani227 <putraahmaddani833@gmail.com>
2025-01-06 22:21:34 +0700

    wip: create home page layout (incomplete)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,82 @@
+import {
+  RiGithubFill,
+  RiInstagramFill,
+  RiLinkedinFill,
+  RiTiktokFill,
+} from "react-icons/ri";
+
 function App() {
   return (
-    <>
-      <h1 className="font-fira">Hello World</h1>
-    </>
+    <div className="border rounded-lg border-midnight-slate bg-midnight-blue">
+      <header className="border-b border-midnight-slate">
+        <nav className="flex justify-between w-full h-[45px]">
+          <div className="flex h-full text-slate-muted">
+            <a href="#" className="nav-link min-w-[275px]">
+              ahmad dani
+            </a>
+            <a href="#" className="nav-link nav-active">
+              _hello
+            </a>
+            <a href="#" className="nav-link">
+              _about-me
+            </a>
+            <a href="#" className="nav-link">
+              _projects
+            </a>
+          </div>
+          <a
+            href="#"
+            className="flex items-center h-full px-6 text-sm border-l text-slate-muted border-midnight-slate"
+          >
+            _contact-me
+          </a>
+        </nav>
+      </header>
+
+      <main className="min-h-[calc(100vh-154px)]">
+        <section className="container">
+          <h1>home section</h1>
+        </section>
+      </main>
+
+      <footer className="border-t border-midnight-slate">
+        <div className="flex justify-between w-full h-[40px]">
+          <div className="flex h-full text-slate-muted">
+            <h1 className="flex items-center h-full px-6 text-sm border-r text-slate-muted border-midnight-slate">
+              find me in:
+            </h1>
+            <a
+              href="#"
+              className="flex items-center h-full px-3 text-sm transition border-r border-midnight-slate group hover:bg-midnight-slate"
+            >
+              <RiInstagramFill className="w-6 h-6 fill-midnight-slate group-hover:fill-slate-muted" />
+            </a>
+            <a
+              href="#"
+              className="flex items-center h-full px-3 text-sm transition border-r border-midnight-slate group hover:bg-midnight-slate"
+            >
+              <RiTiktokFill className="w-6 h-6 fill-midnight-slate group-hover:fill-slate-muted" />
+            </a>
+            <a
+              href="#"
+              className="flex items-center h-full px-3 text-sm transition border-r border-midnight-slate group hover:bg-midnight-slate"
+            >
+              <RiLinkedinFill className="w-6 h-6 fill-midnight-slate group-hover:fill-slate-muted" />
+            </a>
+          </div>
+          <a
+            href="#"
+            target="_blank"
+            className="flex items-center h-full px-6 text-sm transition-all border-l hover:bg-midnight-slate text-slate-muted border-midnight-slate"
+          >
+            @ahmaddani227
+            <span className="ml-1">
+              <RiGithubFill className="w-6 h-6 fill-slate-muted" />
+            </span>
+          </a>
+        </div>
+      </footer>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        lg: "3rem",
+      },
+    },
     extend: {
       colors: {
+        "midnight-slate": "#1E2D3D", // Color lines
+        "midnight-blue": "#011627", // Primary - Background Color
+        "slate-muted": "#607B96", // Secondary - Text Color
+        "soft-orange": "#FEA55F", // Accent - Highlight Color
+        // "dark-blue": "#011627", // Primary - Hover Color
         "gray-dark": "#121212",
         "glitch-green": "#00ff00",
         "glitch-rose": "#f0f",
@@ -14,18 +25,13 @@ export default {
         "neon-blue-l": "#05131e",
         "neon-blue-xl": "#020204",
         "dark-background": "#010C15",
-        "menu-text": "#607B96",
-        "blue-background": "#011627",
         "hello-name": "#E5E9F0",
         "purple-text": "#4D5BCE",
-        "hello-gray": "#607B96",
         "codeline-link": "#E99287",
         "codeline-tag": "#4D5BCE",
         "codeline-name": "#43D9AD",
-        "mobile-menu-blue": "#011627",
         "placeholder-gray": "#465E77",
         greenfy: "#43D9AD",
-        "bluefy-dark": "#011627",
         purplefy: "#5565E8",
       },
       fontFamily: {
