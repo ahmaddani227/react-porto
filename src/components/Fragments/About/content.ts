ba1d6ff44143d9429cd2b1128a979101dd0bc574
ahmaddani227 <putraahmaddani833@gmail.com>
2025-01-22 20:00:50 +0700

    refactor(about): move 'about' page folder

--- src/components/Fragments/About/content.ts
+++ src/components/Fragments/About/content.ts
@@ -1,13 +1,13 @@
-import Experience from "./professional-info/Experience";
-import HardSkills from "./professional-info/HardSkills";
-import SoftSkills from "./professional-info/SoftSkills";
-import Bio from "./personal-info/Bio";
-import Education from "./personal-info/Education";
-import HighSchool from "./personal-info/HighSchool";
-import Interest from "./personal-info/Interest";
-import University from "./personal-info/University";
-import Sports from "./hobbies-info/Sports";
-import FavoriteGame from "./hobbies-info/FavoriteGame";
+import Experience from "../../../pages/About/professional-info/Experience";
+import HardSkills from "../../../pages/About/professional-info/HardSkills";
+import SoftSkills from "../../../pages/About/professional-info/SoftSkills";
+import Bio from "../../../pages/About/personal-info/Bio";
+import Education from "../../../pages/About/personal-info/Education";
+import HighSchool from "../../../pages/About/personal-info/HighSchool";
+import Interest from "../../../pages/About/personal-info/Interest";
+import University from "../../../pages/About/personal-info/University";
+import Sports from "../../../pages/About/hobbies-info/Sports";
+import FavoriteGame from "../../../pages/About/hobbies-info/FavoriteGame";
 
 export {
   Experience,
