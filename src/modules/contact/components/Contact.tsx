c3d783883acb47bc0d80cd57ce593291cb7ba31e
Ahmad Dani <putraahmaddani833@gmail.com>
2025-02-23 12:20:52 +0700

    fix(ui): contact me

--- src/modules/contact/components/Contact.tsx
+++ src/modules/contact/components/Contact.tsx
@@ -34,71 +34,73 @@ const Contact = () => {
   return (
     <>
       <h1 className="py-3 lg:hidden px-fluid">_contact-me</h1>
-      <IDE>
-        <IDE.Sidebar>
-          <div className="flex flex-col w-full">
-            {contacts.map((item) => (
-              <SidebarMenu
-                classname={`${
-                  item.id === "Find Me Also" ? "border-0" : "border-b"
-                }`}
-                key={item.id}
-                toggle={handleToggleFolder}
-                expandedFolders={expandedFolders}
-                data={item}
-              >
-                <div className="py-2 space-y-3 lg:ps-5">
-                  {item.contactsMenu.map((item) => (
-                    <ContactsLink key={item.title} data={item} />
-                  ))}
-                </div>
-              </SidebarMenu>
-            ))}
-          </div>
-        </IDE.Sidebar>
-
-        <IDE.Main>
-          <IDE.TabBar
-            activeTab={activeTab}
-            tabs={tabs}
-            setActiveTab={setActiveTab}
-          />
-          <Content>
-            <div className="flex h-full lg:grid lg:grid-cols-2">
-              <div id="form-contact" className="form-contact-container">
-                <form className="form-contact">
-                  <InputField
-                    onChange={(e) => {
-                      setForm({ ...form, name: e.target.value });
-                    }}
-                    title="_name"
-                    type="text"
-                    name="name"
-                  />
-                  <InputField
-                    onChange={(e) => {
-                      setForm({ ...form, email: e.target.value });
-                    }}
-                    title="_email"
-                    type="enail"
-                    name="email"
-                  />
-                  <InputField
-                    onChange={(e) => {
-                      setForm({ ...form, message: e.target.value });
-                    }}
-                    title="_message"
-                    name="message"
-                    isTextarea={true}
-                  />
-                  <Button>send-message</Button>
-                </form>
-              </div>
-              <FormPreview dataForm={form} />
+      <div className="min-h-[calc(100vh-155px)]">
+        <IDE>
+          <IDE.Sidebar>
+            <div className="flex flex-col w-full">
+              {contacts.map((item) => (
+                <SidebarMenu
+                  classname={`${
+                    item.id === "Find Me Also" ? "border-0" : "border-b"
+                  }`}
+                  key={item.id}
+                  toggle={handleToggleFolder}
+                  expandedFolders={expandedFolders}
+                  data={item}
+                >
+                  <div className="py-2 space-y-3 lg:ps-5">
+                    {item.contactsMenu.map((item) => (
+                      <ContactsLink key={item.title} data={item} />
+                    ))}
+                  </div>
+                </SidebarMenu>
+              ))}
             </div>
-          </Content>
-        </IDE.Main>
-      </IDE>
+          </IDE.Sidebar>
+
+          <IDE.Main>
+            <IDE.TabBar
+              activeTab={activeTab}
+              tabs={tabs}
+              setActiveTab={setActiveTab}
+            />
+            <Content>
+              <div className="flex h-full lg:grid lg:grid-cols-2">
+                <div id="form-contact" className="form-contact-container">
+                  <form className="form-contact">
+                    <InputField
+                      onChange={(e) => {
+                        setForm({ ...form, name: e.target.value });
+                      }}
+                      title="_name"
+                      type="text"
+                      name="name"
+                    />
+                    <InputField
+                      onChange={(e) => {
+                        setForm({ ...form, email: e.target.value });
+                      }}
+                      title="_email"
+                      type="enail"
+                      name="email"
+                    />
+                    <InputField
+                      onChange={(e) => {
+                        setForm({ ...form, message: e.target.value });
+                      }}
+                      title="_message"
+                      name="message"
+                      isTextarea={true}
+                    />
+                    <Button>send-message</Button>
+                  </form>
+                </div>
+                <FormPreview dataForm={form} />
+              </div>
+            </Content>
+          </IDE.Main>
+        </IDE>
+      </div>
     </>
   );
 };
