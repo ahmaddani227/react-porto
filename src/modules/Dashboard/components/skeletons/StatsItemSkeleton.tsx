a778a7b931318fee9c0a4a6dc6ee2fea669be7cd
Ahmad Dani <putraahmaddani833@gmail.com>
2025-11-12 16:15:34 +0700

    feat: add skeleton in github stats

---
+++ src/modules/Dashboard/components/skeletons/StatsItemSkeleton.tsx
@@ -0,0 +1,8 @@
+export default function StatsItemSkeleton() {
+  return (
+    <div className="w-[257px] h-[81px] p-3 border rounded-md animate-pulse border-midnight-slate flex flex-col justify-center">
+      <div className="w-20 h-3 mb-2.5 rounded-full bg-slate-700" />
+      <div className="w-16 h-5 rounded-full bg-slate-700" />
+    </div>
+  );
+}
